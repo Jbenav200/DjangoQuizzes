d470e0a48acb5c215d2089bc8013f8cdbf4b1438
Euan Barrett <ergoeuan@gmail.com>
2019-03-24 20:00:56 +0000

    Worked on whole project

--- FDM_quiz/quiz/urls.py
+++ FDM_quiz/quiz/urls.py
@@ -1,6 +1,9 @@
-from django.conf.urls import url
-from .views import MainPage
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    url(r'^$', MainPage.as_view(), name='index'),
+    path('', views.home, name='quiz-home'),
+    path('user_home/', views.user_home, name='quiz-user_home'),
+    path('report/', views.report, name='quiz-report'),
+    path('questions/', views.questions, name='quiz-questions'),
 ]
--- FDM_quiz/quiz/views.py
+++ FDM_quiz/quiz/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import View
-from django.contrib.auth.mixins import AccessMixin
 
+
+def home(request):
+    return render(request, 'quiz/home.html')
+
+
+def user_home(request):
+    return render(request, 'quiz/user_home.html', {'title': 'Home'})
+
+
+def report(request):
+    return render(request, 'quiz/report.html', {'title': 'Report'})
+
+
+def questions(request):
+    return render(request, 'quiz/questions.html', {'title': 'Questions'})
 
 # GET -> asks for stuff
 # POST -> sends you stuff
 # PUT -> very specific stuff or if you are a webdev from the 1990s
 # HEAD -> headers
-class MainPage(AccessMixin, View):
-    def get(self, request):
-        return render(
-            request, "main_page.html", {"name": "Euan"})
