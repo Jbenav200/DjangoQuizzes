a7bf0f8a244d909ff4524a17224dd278936f4e43
Euan Barrett <ergoeuan@gmail.com>
2019-03-26 01:12:01 +0000

    Register system finished

--- FDM_quiz/FDM_quiz/settings.py
+++ FDM_quiz/FDM_quiz/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'quiz.apps.QuizConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,4 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-ALLOWED_HOSTS = ['*']
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- README.md
+++ README.md
@@ -4,4 +4,5 @@ IP2 Web Project
 Inside /IP2/FDM_quiz
 python manage.py runserver 127.0.0.1:8080
 
-admin password: O9v#TyO9IhW0
+Euan home pc admin password: O9v#TyO9IhW0
+
