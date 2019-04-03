1815f8510f3fb28d1ae071ba352ddd265e459cd8
Euan Barrett <ergoeuan@gmail.com>
2019-04-03 15:27:56 +0100

    First Implement

--- FDM_quiz/quiz/models.py
+++ FDM_quiz/quiz/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from .models import *
+
 
 # Create your models here.
+# Category model
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+# Question model
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    correct_answer = models.CharField(max_length=200)
+    published_date = models.DateTimeField('date published')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.question_text
+
+
+# Choice model
+class Choice(models.Model):
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.choice_text
+
+
+# User model
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
--- FDM_quiz/quiz/views.py
+++ FDM_quiz/quiz/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.template import loader
+from .models import *
 
 
 def home(request):
@@ -12,12 +15,36 @@ def about(request):
 
 @login_required
 def quizzes(request):
-    return render(request, 'quiz/quizzes.html', {'title': 'Quizzes'})
+    questions_list = Question.objects.order_by('question_text')
+    choices_list = Choice.objects.order_by('question')
+    categories_list = Category.objects.order_by('name')
+    template = loader.get_template('quiz/questions.html')
+    context = {
+        'questions_list': questions_list,
+        'title': "Questions",
+        'choices_list': choices_list,
+        'categories_list': categories_list,
+
+    }
+    return HttpResponse(template.render(context, request))
+    # return render(request, 'quiz/quizzes.html', {'title': 'Quizzes'})  (idk what you do with your returns but try work my one in as well)
 
 
 @login_required
 def results(request):
-    return render(request, 'quiz/results.html', {'title': 'Results'})
+    category_list = Category.objects.order_by('name')
+    user_list = User.objects.order_by('username')
+    from FDM_quiz import users
+    username = users.models.User.return_username()
+    template = loader.get_template("quiz/reports.html")
+    context = {
+        'category_list': category_list,
+        'user_list': user_list,
+        'title': "Report",
+        'username': username,
+    }
+    return HttpResponse(template.render(context, request))
+    # return render(request, 'quiz/results.html', {'title': 'Results'}) (idk what you do with your returns but try work my one in as well)
 
 
 @login_required
